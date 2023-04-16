d85c718d8f7443e6388ff39d75b2b02c5b47a574
nizarui <105335703+nizarui@users.noreply.github.com>
2023-04-16 22:30:46 +0700

    Add files via upload

---
+++ praktikum6/tugas3/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Biodata</title>
+</head>
+
+<body>
+    <h1>BIODATA DIRI</h1>
+    <form method="post" action="proses.php">
+        <table>
+            <input type="image" src="" alt="">
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text" name="nama" required></td>
+            </tr>
+            <tr>
+                <td>Umur</td>
+                <td>:</td>
+                <td><input type="number" name="umur" required></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>:</td>
+                <td>
+                    <input type="radio" name="jk" value="Laki-laki" required>Laki-laki
+                    <input type="radio" name="jk" value="Perempuan" required>Perempuan
+                </td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td><textarea name="alamat" required></textarea></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type="submit" name="submit" value="Simpan"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
