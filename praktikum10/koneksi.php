2d187fce124c169bd5f5396729ee998e4576f680
nizarui <105335703+nizarui@users.noreply.github.com>
2023-05-27 13:06:03 +0700

    Delete praktikum10 directory

--- praktikum10/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "db_penjualan";
-$koneksi    = mysqli_connect($host, $user, $password, $database);
-?>
--- praktikum10/koneksi1.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$host       = "localhost";
-$user       = "root";
-$password   = "";
-$database   = "covid";
-$koneksi    = mysqli_connect($host, $user, $password, $database);
-?>
